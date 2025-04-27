400d0263a7fd9fe70d4cae10b641edf783536c29
ganta kokila <gantakokilaa@gmail.com>
2025-04-27 10:57:33 +0530

    Initial commit with app.py, templates, static, and model

--- app.py
+++ app.py
@@ -203,5 +203,8 @@ def predict():
 
 # @app.route('/result', methods=['POST'])
 # ------------------ Run the App ------------------
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
